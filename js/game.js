23c2c463900e82e2d1706b881879fcc23695d92b
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2018-02-07 21:53:00 +0100

    Etadpu

--- js/game.js
+++ js/game.js
@@ -1,10 +1,20 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+	game.load.image("bg", "img/bg.png");
+	game.scale.setMaximum();
 }
 
 function create() {
+	var bg = game.add.sprite(0, 0, 'bg');
+	//bg.scale.setTo(window.innerWidth, window.innerHeight);
+	
+	/*scaleRatio = window.devicePixelRatio / 3;
+	bg.scale.setTo(scaleRatio, scaleRatio);//*/
+	bg.width = window.innerWidth * window.devicePixelRatio;
+	bg.height = window.innerHeight * window.devicePixelRatio;
 }
 
 function update() {
+	
 }
