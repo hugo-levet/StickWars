c2611d4af03bd62559115e5fcbcc5c682c2ec6a3
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2018-02-07 20:54:08 +0100

    Update

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Ceci est un projet bien cool !
 
 Bibliothèques utilisées pour le projet:	
-	-> Physique et collision: http://box2d-js.sourceforge.net/
+-> Physique et collision: Box2D
+	Lien: http://box2d-js.sourceforge.net/
+	Potentielle documentation (la doc est pour du C): http://box2d.org/manual.pdf
--- js/game.js
+++ js/game.js
@@ -8,7 +8,36 @@ background.src = "img/bg.png";
 c.width  = window.innerWidth - 90;
 c.height = c.width * background.height / background.width;//*/
 
-// on attends que l'image bg.png soit chargé avant de l'afficher
-background.onload = function() {
-    ctx.drawImage(background, 0, 0, c.width, c.height);   
+// ====
+// on attends que les ressources soient téléchargés avant d'initialiser le jeu
+background.onload = function() {    
+
+	// on créer un monde
+	var worldAABB = new b2AABB();
+	worldAABB.minVertex.Set(-1000, -1000);
+	worldAABB.maxVertex.Set(1000, 1000);
+	var gravity = new b2Vec2(0, 300);
+	var doSleep = true;
+	var world = new b2World(worldAABB, gravity, doSleep); 
+	
+	// on créer une boulasse
+	var circleSd = new b2CircleDef();
+	circleSd.density = 1.0;
+	circleSd.radius = 20;
+	circleSd.restitution = 1.0;
+	circleSd.friction = 0;
+	var circleBd = new b2BodyDef();
+	circleBd.AddShape(circleSd);
+	circleBd.position.Set(0,0);
+	var circleBody = world.CreateBody(circleBd);
+	
+	// on lance la boucle principale
+	main();
+}
+
+// ====
+// la boucle principale
+function main() {	
+	ctx.drawImage(background, 0, 0, c.width, c.height);   
+	main();
 }
