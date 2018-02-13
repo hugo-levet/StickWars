b50091eca3397bcb7c5cb14f6bd088cd2a45e3d4
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2018-02-13 14:07:47 +0100

    UI update

--- js/states/menu.js
+++ js/states/menu.js
@@ -10,22 +10,36 @@ var Menu = {
         
         game.stage.backgroundColor = '#182d3b';
         
+        // Le titre
+        var text = game.add.bitmapText(game.world.centerX, 200, 'pixel', 'StickWars', 32);
+        text.anchor.setTo(.5,.5);
+        
+        // PLAY BUTTON
         // créer l'image du bouton
         var play = game.add.button(game.world.centerX, 400, 'button', loadGame, this, 2, 1, 0);
         play.anchor.setTo(.5,.5);
-        play.position.setTo(game.world.centerX, 400);
         
         // puis on pose un texte par dessus
         var text = game.add.bitmapText(game.world.centerX, 400, 'pixel', 'Play', 32);
         text.anchor.setTo(.5,.5);
         
-
+        // CREDITS BUTTON
+        // créer l'image du bouton
+        var play = game.add.button(game.world.centerX, 550, 'button', loadGame, this, 2, 1, 0);
+        play.anchor.setTo(.5,.5);
+        
+        // puis on pose un texte par dessus
+        var text = game.add.bitmapText(game.world.centerX, 550, 'pixel', 'Credits', 32);
+        text.anchor.setTo(.5,.5);
     },
 };
 
 function loadGame() {
-
     game.state.add('Game', Game);
     game.state.start('Game');
+}
 
+function loadCredits() {
+    game.state.add('Credits', Credits);
+    game.state.start('Credits');
 }
