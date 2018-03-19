0e57909791dd21ad8a6f9b51c60159954952adac
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2018-03-19 18:53:01 +0100

    Responsive design pour le menu

--- js/states/main_menu.js
+++ js/states/main_menu.js
@@ -5,25 +5,29 @@ var MainMenu = {
         game.stage.backgroundColor = '#182d3b';
                 
         // Le titre
-        var text = game.add.bitmapText(game.world.centerX, 200, 'pixel', 'StickWars', 32);
+        var text = game.add.bitmapText(game.world.centerX, convertY(200), 'pixel', 'StickWars', convertY(32));
         text.anchor.setTo(.5,.5);
+        
         
         // PLAY BUTTON
         // créer l'image du bouton
-        var play = game.add.button(game.world.centerX, 400, 'button', loadPlayMenu, this, 2, 1, 0);
+        var play = game.add.button(game.world.centerX, convertY(400), 'button', loadPlayMenu, this, 2, 1, 0);
         play.anchor.setTo(.5,.5);
+        play.scale.setTo(ratioX, ratioY);
+        //play.height = convertY(play.height);
         
         // puis on pose un texte par dessus
-        var text = game.add.bitmapText(game.world.centerX, 400, 'pixel', 'Play', 32);
+        var text = game.add.bitmapText(game.world.centerX, convertY(400), 'pixel', 'Play', convertY(32));
         text.anchor.setTo(.5,.5);
         
         // CREDITS BUTTON
         // créer l'image du bouton
-        var play = game.add.button(game.world.centerX, 550, 'button', loadCredits, this, 2, 1, 0);
+        var play = game.add.button(game.world.centerX, convertY(550), 'button', loadCredits, this, 2, 1, 0);
         play.anchor.setTo(.5,.5);
+        play.scale.setTo(ratioX, ratioY);
         
         // puis on pose un texte par dessus
-        var text = game.add.bitmapText(game.world.centerX, 550, 'pixel', 'Credits', 32);
+        var text = game.add.bitmapText(game.world.centerX, convertY(550), 'pixel', 'Credits', convertY(32));
         text.anchor.setTo(.5,.5);
     },      
     
