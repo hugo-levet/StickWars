04986e3b7b71e9a4f266d60d9ee7f43b57eaf367
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2018-03-22 22:28:05 +0100

    On améliore la beauté de la barre

--- js/states/preload.js
+++ js/states/preload.js
@@ -3,15 +3,20 @@ var Preload = {
     
     // ici on charge toutes les ressources pour l'état actuel (images, sons, etc...)
     preload: function () {
+		
 		game.stage.backgroundColor = '#182d3b';      
 		
+		
+		// on créer la barre de chargement
 		console.log("In preload state");
-				
+		
 		var logo = game.add.sprite(game.world.centerX, game.world.centerY, "logo");
 		logo.anchor.setTo(0.5);				
 		
-		var preloadBar = game.add.sprite(0, game.world.centerY - 50, "preload_bar");
-		preloadBar.width = width;		
+		var preloadBar = game.add.sprite(game.world.centerX, convertY(35), "preload_bar");		
+		preloadBar.anchor.setTo(0.5);				
+		preloadBar.width -= convertX(5);		
+		preloadBar.height = convertY(10);
 		game.load.setPreloadSprite(preloadBar);
 		
 		
